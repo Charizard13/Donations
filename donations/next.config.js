d5945f2d68d3a938a615278d6a81d880dc2193c4
Tal Piven <67857533+Charizard13@users.noreply.github.com>
2023-10-14 18:06:28 +0300

    Added font

--- donations/next.config.js
+++ donations/next.config.js
@@ -4,7 +4,11 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['northarm.co.il'],
-  }
+  },
+  i18n: {
+    locales: ['en-US', 'he-IL'],
+    defaultLocale: 'he-IL',
+  },
 };
 
 module.exports = nextConfig;
--- donations/src/app/layout.tsx
+++ donations/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import ThemeRegistry from "@/components/ThemeRegistry/ThemeRegistry";
 import { Sheet } from "@mui/joy";
+import { Noto_Sans_Hebrew } from "next/font/google";
 
 import Navbar from "@/ui/NavBar";
 
@@ -8,10 +9,15 @@ export const metadata = {
   title: "עוברים את זה ביחד",
   description: "עוברים את זה ביחד",
 };
+// If loading a variable font, you don't need to specify the font weight
+const NotoSansHebrew = Noto_Sans_Hebrew({
+  subsets: ["hebrew"],
+  display: "swap",
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="he" dir="rtl">
+    <html lang="he" dir="rtl" className={NotoSansHebrew.className}>
       <body>
         <ThemeRegistry>
           <Sheet
